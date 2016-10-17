9008d61622e29fe6f4552892b74d2bb476a784d9
kamil.chlebek <kamil.chlebek@salesmanago.pl>
2016-10-18 01:10:20 +0200

    config

--- adly-auth/src/main/java/pl/edu/agh/student/AdlyAuthApplication.java
+++ adly-auth/src/main/java/pl/edu/agh/student/AdlyAuthApplication.java
@@ -3,6 +3,7 @@ package pl.edu.agh.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
--- adly-edge/src/main/java/pl/edu/agh/student/AdlyEdgeApplication.java
+++ adly-edge/src/main/java/pl/edu/agh/student/AdlyEdgeApplication.java
@@ -1,20 +1,33 @@
 package pl.edu.agh.student;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignAutoConfiguration;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.hateoas.Resources;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableResourceServer
 @EnableHystrix
+@ImportAutoConfiguration(FeignAutoConfiguration.class)
+@EnableFeignClients
 public class AdlyEdgeApplication  {
 
 
@@ -29,40 +42,40 @@ public class AdlyEdgeApplication  {
 	}
 }
 
-//@FeignClient("adly-reservation-service")
-//interface UserClientV1 {
-//	@RequestMapping(method = RequestMethod.GET, value = "/reservations")
-//	Resources<Reservations> read();
-//}
-//
+@FeignClient("adly-reservation-service")
+interface UserClientV1 {
+	@RequestMapping(method = RequestMethod.GET, value = "/reservations")
+	Resources<Reservations> read();
+}
+
 //@FeignClient("adly-auth")
 //interface AuthClient {
 //    @RequestMapping(method = RequestMethod.GET, value = "/user")
 //    Resource<String> read();
 //}
 //
-//@RestController
-//@RequestMapping("/reservations")
-//class ReservationApiGatewayRestController {
-//
-//    private final UserClientV1 userClientV1;
-//
-//    @Autowired
-//    public ReservationApiGatewayRestController( UserClientV1 userClientV1) {
-//        this.userClientV1 = userClientV1;
-//    }
-//
-//    public Collection<String> fallback(){
-//        return new ArrayList();
-//    }
-//
-//    @RequestMapping("/names")
-//    @HystrixCommand(fallbackMethod = "fallback")
-//    public Collection<String> names(){
-//
-//        return userClientV1.read().getContent().stream().map(Reservations::getReservationName).collect(Collectors.toList());
-//    }
-//}
+@RestController
+@RequestMapping("/reservations")
+class ReservationApiGatewayRestController {
+
+    private final UserClientV1 userClientV1;
+
+    @Autowired
+    public ReservationApiGatewayRestController(UserClientV1 userClientV1) {
+        this.userClientV1 = userClientV1;
+    }
+
+    public Collection<String> fallback(){
+        return new ArrayList();
+    }
+
+    @RequestMapping("/names")
+    @HystrixCommand(fallbackMethod = "fallback")
+    public Collection<String> names(){
+
+        return userClientV1.read().getContent().stream().map(Reservations::getReservationName).collect(Collectors.toList());
+    }
+}
 //
 //@RestController
 //class MeController {
@@ -84,14 +97,14 @@ public class AdlyEdgeApplication  {
 //        return authClient.read().getContent();
 //    }
 //}
-//class Reservations {
-//	private String reservationName;
-//
-//	public String getReservationName() {
-//		return reservationName;
-//	}
-//
-//	public void setReservationName(String reservationName) {
-//		this.reservationName = reservationName;
-//	}
-//}
+class Reservations {
+	private String reservationName;
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+}
