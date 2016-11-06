0128579b09fbee1f65265345ec5a37d094604175
kamil.chlebek <kamil.chlebek@salesmanago.pl>
2016-11-06 20:06:03 +0100

    actions++, condition ++

--- adly-rule-service/src/main/java/pl/edu/agh/student/services/EventHandler.java
+++ adly-rule-service/src/main/java/pl/edu/agh/student/services/EventHandler.java
@@ -50,15 +50,6 @@ public class EventHandler {
 
         LOG.info("Processing rules finished in: " + (System.currentTimeMillis() - startMillis));
 
-        //@todo:
-        /*
-         *   *** 1. wyciagnac z bazy by eventType
-         *   2. przegladnac petla elementy i sprawdzic czy parametry z bazy odpowiadaja parametrom eventu usera
-         *   ( zamknac to w jakiejs osobnej klasie)
-         *   3. Dla kazdego warunku zrobić evaluate (osobne komponenty w zaleznosci od typu warunku)
-         *   4. Dla każdej akcji wykonać execute ( np. SendPush executor rzuci na kolejke push)
-         *   5. done (+sprawdzic, czy push payload jest dostepny z zewnatrz, a pewnie nie jest bo nie zapisuje nigdzie pusha na bazie)
-         */
     }
 
 
