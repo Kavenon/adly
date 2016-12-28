2bc278fadeda9cd7403f33630576ecc383c814d3
kamil.chlebek <kamil.chlebek@salesmanago.pl>
2016-12-28 14:49:26 +0100

    docker working

--- adly-edge/src/main/java/pl/edu/agh/student/AdlyEdgeApplication.java
+++ adly-edge/src/main/java/pl/edu/agh/student/AdlyEdgeApplication.java
@@ -2,12 +2,10 @@ package pl.edu.agh.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
 @EnableHystrix
 public class AdlyEdgeApplication  {
